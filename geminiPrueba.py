997723b292a4d83c50179397cda5453ac2b2c132
Victoria <a00836008@tec.mx>
2025-11-26 22:37:15 -0600

    api key change

--- geminiPrueba.py
+++ geminiPrueba.py
@@ -2,7 +2,7 @@ from google import genai
 from dotenv import load_dotenv
 import os
 import json
-import streamlit as st
+# import streamlit as st
 
 # st.write("La API key existe:", "GEMINI_API_KEY" in st.secrets)
 
